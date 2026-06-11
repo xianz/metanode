171fbfca560cbf99629b7c6694c846ead02d4708
x1pro <lxianzi@qq.com>
2026-06-12 03:06:08 +0800

    jwt

--- lesson-03/05-jwt-auth/main.go
+++ lesson-03/05-jwt-auth/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -10,13 +12,14 @@ import (
 )
 
 type Claims struct {
+	UserID   int    `json:"user_id"`
 	Username string `json:"username"`
-	Password string `json:"password"`
 	jwt.RegisteredClaims
 }
 
 var jwtSecret = []byte("my-secret-key")
 
+// ======== generateToken 生成JWT token
 func generateToken(userID int, username string) (string, error) {
 	claims := Claims{
 		UserID:   userID,
@@ -30,45 +33,53 @@ func generateToken(userID int, username string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString(jwtSecret)
-	// claims := jwt.MapClaims{
-	// 	"user_id":  userID,
-	// 	"username": username,
-	// 	"exp":      time.Now().Add(time.Hour * 24).Unix(),
-	// }
-	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	// return token.SignedString(jwtSecret)
 }
 
-// func generateToken(username string) (string, error) {
-// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
-// 		Username: username,
-// 		Password: "password",
-// 		RegisteredClaims: jwt.RegisteredClaims{
-// 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
-// 			IssuedAt:  jwt.NewNumericDate(time.Now()),
-// 		},
-// 	})
-// 	return token.SignedString(jwtSecret)
-// }
+// ========  解析 Token ==========
+func parseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
 
 func main() {
 	fmt.Println("JWT Auth")
 	r := gin.Default()
-	r.GET("/login", func(c *gin.Context) {
-		c.String(200, "Login page")
-	})
+	r.GET("/login", login)
 	v1 := r.Group("v1")
 	v1.Use(authMiddleware())
 	v1.GET("/protected", func(c *gin.Context) {
 		c.String(200, "Protected page")
 	})
+	v1.GET("/test", func(c *gin.Context) {
+		userID, _ := c.Get("userID")
+		username, _ := c.Get("username")
+		c.JSON(200, gin.H{
+			"userID":   userID,
+			"username": username,
+		})
+	})
+	r.Run(":8080")
 }
 
 // ========== 认证中间件（示例） ==========
 func authMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.GetHeader("Authorization")
-		if token == "" {
+		headerAuth := c.GetHeader("Authorization")
+		if headerAuth == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"error": "Authorization header required",
 			})
@@ -76,8 +87,20 @@ func authMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		// 模拟验证 Token
-		if token != "Bearer valid-token" {
+		////验证 Token
+		// 提取
+		parts := strings.Split(headerAuth, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid authorization header format",
+			})
+			c.Abort()
+			return
+		}
+		// 验证
+		tokenString := parts[1]
+		claims, err := parseToken(tokenString)
+		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"error": "Invalid token",
 			})
@@ -86,9 +109,63 @@ func authMiddleware() gin.HandlerFunc {
 		}
 
 		// 将用户信息存储到 Context
-		c.Set("userID", 1)
-		c.Set("username", "admin")
+		c.Set("userID", claims.UserID)
+		c.Set("username", claims.Username)
 
 		c.Next()
 	}
+}
+
+func login(c *gin.Context) {
+	//获取请求参数
+	var req struct {
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+
+	/*** 拦截打印一下 *
+	bodyBytes, err := c.GetRawData()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read request body",
+		})
+		return
+	}
+	log.Printf("request body: %s", string(bodyBytes))
+	c.Request.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	// */
+
+	// 解析到json对象
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errorsss": err.Error(),
+		})
+		return
+	}
+	// TODO: 验证用户名和密码
+	if req.Username != "admin" || req.Password != "abc" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Invalid username or password",
+		})
+		return
+	}
+	db_user_id := 1001
+
+	//生成token
+	token, err := generateToken(db_user_id, req.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to generate token",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+		"user": gin.H{
+			"userID":   db_user_id,
+			"username": req.Username,
+		},
+	})
+
 }
